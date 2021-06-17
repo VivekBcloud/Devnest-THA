0f7672b17f03d75c8712a778f285ce1f875f735c
VivekBcloud <20vivektoppo@gmail.com>
2021-06-17 22:01:45 +0530

    few improvement

--- Frontend/day-9/app.js
+++ Frontend/day-9/app.js
@@ -1,5 +1,8 @@
 const colorGrid = document.querySelector(".color-grid");
-
+const bookedSeatText = document.getElementById("bs");
+const availSeatText = document.getElementById("avs");
+var bookedSeats = 0;
+var remSeats = 36;
 // console.log(colorGrid);
 function fillTheBox() {
   let div = document.createElement("div");
@@ -10,6 +13,10 @@ function fillTheBox() {
   div.classList.add("pixi");
   return div;
 }
+const showChanges = () => {
+  bookedSeatText.innerText = `Booked:${bookedSeats}`;
+  availSeatText.innerText = `Available Seats:${remSeats}`;
+};
 
 let cnt = 36;
 while (cnt) {
@@ -17,6 +24,15 @@ while (cnt) {
   cnt--;
 }
 colorGrid.addEventListener("click", (e) => {
-  console.log(e);
-  e.target.classList.toggle("changeColor");
+  //   console.log(e);
+  if (e.target.classList.contains("changeColor")) {
+    e.target.classList.remove("changeColor");
+    bookedSeats--;
+    remSeats++;
+  } else {
+    e.target.classList.add("changeColor");
+    bookedSeats++;
+    remSeats--;
+  }
+  showChanges();
 });
