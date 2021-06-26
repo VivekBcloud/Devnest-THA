a8156f97035df02f2840385c1fb5bdc0d060d779
VivekBcloud <20vivektoppo@gmail.com>
2021-06-26 19:05:19 +0530

    done THA12 🌪

--- Frontend/day-12/script.js
+++ Frontend/day-12/script.js
@@ -3,8 +3,13 @@ addbtn = document.querySelector("#addbtn");
 todolist = document.querySelector(".todolist");
 // console.log(addbtn);
 
+inputtext.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13) {
+    e.preventDefault();
+    addbtn.click();
+  }
+});
 const deletetodoitem = (item) => {
-  console.log(item);
   let listtodel = item.target.parentNode;
   listtodel.remove();
   //   console.log(listtodel);
@@ -12,12 +17,14 @@ const deletetodoitem = (item) => {
 };
 const completetodoitem = (item) => {
   //   console.log(item);
-  let listtodel = item.target.parentNode;
-  listtodel.classList.add("completed");
+  let listtomark = item.target.parentNode;
+  // console.log(listtomark);
+  listtomark.classList.add("completed");
+  listtomark.firstChild.classList.add("completed");
   storelocally();
 };
 
-const createtodoitem = (item) => {
+const createtodoitem = (item, status = false) => {
   containerTodo = document.createElement("div");
   containerTodo.classList.add("itemcontainer");
   todo = document.createElement("div");
@@ -31,6 +38,11 @@ const createtodoitem = (item) => {
   compbtn.addEventListener("click", completetodoitem);
   todo.classList.add("todoitem");
   todo.innerHTML = item;
+
+  if (status == "true") {
+    todo.classList.add("completed");
+    containerTodo.classList.add("completed");
+  }
   containerTodo.append(todo);
   containerTodo.append(compbtn);
   containerTodo.append(delbtn);
@@ -40,7 +52,6 @@ const createtodoitem = (item) => {
 const addtodolist = (e) => {
   if (inputtext.value == "") return;
   inputtext.focus();
-  console.log(inputtext.value);
   createtodoitem(inputtext.value);
   inputtext.value = "";
   storelocally();
@@ -48,11 +59,14 @@ const addtodolist = (e) => {
 
 const storelocally = () => {
   localStorage.clear();
-  items = document.querySelectorAll(".todoitem");
+  items = document.querySelectorAll(".itemcontainer");
   let cnt = 1;
   items.forEach((i) => {
-    console.log(i.innerHTML);
-    localStorage.setItem(`item${cnt}`, i.innerHTML);
+    // console.log(i.firstChild.innerHTML);
+    let status = i.classList.contains("completed");
+    // console.log(status);
+    let val = [`${i.firstChild.innerHTML}, ${status}`];
+    localStorage.setItem(`item${cnt}`, val);
     cnt++;
   });
   console.log(items);
@@ -63,8 +77,11 @@ const showtodolist = () => {
 
   while (listlength) {
     let item = localStorage.getItem(`item${listlength}`);
+    // console.log("item is", typeof item);
+    let itemstr = item.split(", ");
+    // let status = itemstr[1].slice(0);
+    createtodoitem(itemstr[0], itemstr[1]);
     listlength--;
-    createtodoitem(item);
   }
 };
 
