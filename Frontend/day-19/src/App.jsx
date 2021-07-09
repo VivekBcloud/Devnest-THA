b26079fe2a88b16081e59a8868460f7144024943
VivekBcloud <20vivektoppo@gmail.com>
2021-07-09 23:54:41 +0530

    day19 done

--- Frontend/day-19/src/App.jsx
+++ Frontend/day-19/src/App.jsx
@@ -1,7 +1,24 @@
 import "./style.css";
-
+import { useState } from "react";
 const App = () => {
-  return <div></div>;
+  const CounterButton = () => {
+    const [count, setCount] = useState(0);
+    // console.log("yeep");
+    return (
+      <button onClick={() => setCount(count + 1)} className="btn">
+        {count}
+      </button>
+    );
+  };
+  // setCount((count += 1));
+  return (
+    <div className="container">
+      <CounterButton />
+      <CounterButton />
+      <CounterButton />
+      <CounterButton />
+    </div>
+  );
 };
 
 export default App;
