a57d63132186b83fddba41a91a2bebeeac1fb9fe
VivekBcloud <20vivektoppo@gmail.com>
2021-07-27 15:15:50 +0530

    Added the styling 🎭

--- Frontend/day-26/src/App.jsx
+++ Frontend/day-26/src/App.jsx
@@ -9,23 +9,26 @@ function App() {
   const dispatch = useDispatch();
   return (
     <>
+      <h1 className="first-heading">User Form using Redux</h1>
       <div className="App">
         <input
           type="text"
+          placeholder="First Name"
           onChange={(e) => {
             dispatch(firstNameInput(e.target.value));
           }}
         />
         <input
           type="text"
+          placeholder="Last Name"
           onChange={(e) => {
             dispatch(lastNameInput(e.target.value));
           }}
         />
       </div>
       <div className="show-names">
-        <div>{firstName}</div>
-        <div>{lastName}</div>
+        <div>First Name : {firstName}</div>
+        <div>Last Name : {lastName}</div>
       </div>
     </>
   );
