390c56e81a43cb7fc210f03f84dc6642236163f1
VivekBcloud <20vivektoppo@gmail.com>
2021-09-23 23:32:40 +0530

    day-9 done 🔩

--- Backend/day-8/index.js
+++ Backend/day-8/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("time", Date.now());
+  next();
+});
+
 app.get("/getdata", (req, res) => {
   res.statusCode = 200;
   res.json("Hello");
---
+++ Backend/day-9/routes/users.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.get("/:files/:name", (req, res) => {
+  const { files, name } = req.params;
+  console.log(name);
+  const src = path.join(__dirname, `../public/${files}/${name}`);
+  res.sendFile(src);
+});
+
+module.exports = router;
